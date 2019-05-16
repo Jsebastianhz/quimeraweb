406dcd47ecc1e4e80434c5215597f9374fc280b2
Sebastian Hernandez <sebaslbp19972015@gmail.com>
2019-05-16 16:08:08 -0500

    Body

--- sitio/src/app/app.module.ts
+++ sitio/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BodyComponent } from './paginas/body/body.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    BodyComponent,
 
 
   ],
